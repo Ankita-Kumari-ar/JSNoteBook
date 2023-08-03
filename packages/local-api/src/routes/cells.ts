6cdb16d3d7b04c9a34522a369dcb45906b160eab
Ankita Kumari <ankita.ashreet@gmail.com>
2023-08-03 15:46:15 +0530

    local-api modified

--- packages/local-api/src/routes/cells.ts
+++ packages/local-api/src/routes/cells.ts
@@ -1,11 +1,11 @@
-import express from 'express';
-import fs from 'fs/promises';
-import path from 'path';
+import express from "express";
+import fs from "fs/promises";
+import path from "path";
 
 interface Cell {
   id: string;
   content: string;
-  type: 'text' | 'code';
+  type: "text" | "code";
 }
 
 interface LocalApiError {
@@ -17,13 +17,13 @@ export const createCellsRouter = (filename: string, dir: string) => {
   router.use(express.json());
 
   const fullPath = path.join(dir, filename);
-  router.get('/cells', async (request, response) => {
+  router.get("/cells", async (request, response) => {
     const isLocalApiError = (err: any): err is LocalApiError => {
-      return typeof err.code === 'string';
+      return typeof err.code === "string";
     };
     try {
       // Read the file
-      const result = await fs.readFile(fullPath, { encoding: 'utf-8' });
+      const result = await fs.readFile(fullPath, { encoding: "utf-8" });
 
       response.send(JSON.parse(result));
     } catch (err) {
@@ -31,10 +31,10 @@ export const createCellsRouter = (filename: string, dir: string) => {
       // Inspect the error, see if it says that the file doesn't exists
 
       if (isLocalApiError(err)) {
-        if (err.code === 'ENOENT') {
+        if (err.code === "ENOENT") {
           // ENOENT stands for error no entity
           // Add code to create a file and add default cells
-          await fs.writeFile(fullPath, '[]', 'utf-8');
+          await fs.writeFile(fullPath, "[]", "utf-8");
           response.send([]);
         }
       } else {
@@ -43,15 +43,33 @@ export const createCellsRouter = (filename: string, dir: string) => {
     }
   });
 
-  router.post('/cells', async (request, response) => {
+  router.post("/cells", async (request, response) => {
     // Take the list of cells from the request object
     // serialize them
     const { cells }: { cells: Cell[] } = request.body;
 
     // Write the cells into the file
-    await fs.writeFile(fullPath, JSON.stringify(cells), 'utf-8');
+    const comment = "//";
+    let fileData = "";
 
-    response.send({ status: 'ok' });
+    for (let cell of cells) {
+      if (cell.content) {
+        if (cell.type === "text") {
+          fileData +=
+            "\n" + comment + cell.content.replaceAll("\n", "\n" + comment);
+        } else {
+          fileData += "\n\n" + cell.content;
+        }
+      }
+    }
+
+    // console.log(cells);
+    // await fs.writeFile(fullPath, JSON.stringify(cells), "utf-8");
+
+    console.log(fileData);
+    await fs.writeFile(fullPath, fileData, "utf-8");
+
+    response.send({ status: "ok" });
   });
   return router;
 };
